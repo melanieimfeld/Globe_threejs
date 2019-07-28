86214342dabf788fd54f91c5c98f03b19f5f9ed8
melanieimfeld <melanie@imfeld.info>
2019-07-28 17:07:44 +0200

    final changes

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@
 //https://medium.com/@xiaoyangzhao/drawing-curves-on-webgl-globe-using-three-js-and-d3-draft-7e782ffd7ab
 
 const allDonors = ['Japan', 'Germany', 'United States', 'United Kingdom', 'Canada',
-       'Australia', 'Italy', 'Belgium', 'Sweden', 'France', 'Netherlands',
-       'Norway', 'Switzerland', 'Kuwait', 'United Arab Emirates',
-       'Denmark', 'Saudi Arabia', 'Austria', 'Spain', 'Finland', 'Korea'];
-
-const CHcoords = ['47.3673', '8.55'];
+'Australia', 'Italy', 'Belgium', 'Sweden', 'France', 'Netherlands',
+'Norway', 'Switzerland', 'Kuwait', 'United Arab Emirates',
+'Denmark', 'Saudi Arabia', 'Austria', 'Spain', 'Finland', 'Korea'];
 
 //const GLOBE_RADIUS = 0.5;
 var controls, counter=0;
@@ -19,7 +17,6 @@ var loader = new THREE.FileLoader();
 var objectLoader = new THREE.ObjectLoader();
 var jsonLoader = new THREE.JSONLoader();
 
-var radius = 6371;
 var radius1 = 0.6;
 const altitude = 0.4;
 var animIsRunning = true;
@@ -36,28 +33,28 @@ var lightHolder = new THREE.Group();
 
 var remappedScale = d3.scaleLinear().range([0.002, 0.006])
 remappedScale.domain([0.1, 3.85]);
-console.log("remapped scale", remappedScale(3));
+//console.log("remapped scale", remappedScale(3));
 
 
 
 
-    var onRenderFcts= [];
-    var scene = new THREE.Scene();
+var onRenderFcts= [];
+var scene = new THREE.Scene();
 
-    var geometry   = new THREE.SphereGeometry(radius1, 50, 50)
+var geometry   = new THREE.SphereGeometry(radius1, 50, 50)
   // var material  = new THREE.MeshPhongMaterial({
   //  map: texture})
-   var elmt = document.getElementById("earth");
+  var elmt = document.getElementById("earth");
 
    //console.log('windowwidth',window.innerWidth, 'windowheight', window.innerHeight)
    //console.log(elmt,'elementwidth',elmt.clientWidth, 'elementheight', elmt.clientHeight,'element divided', elmt.clientWidth / elmt.clientHeight)
 
-    var material  = new THREE.MeshStandardMaterial()
-    var camera  = new THREE.PerspectiveCamera(45, elmt.clientWidth / window.innerHeight, 0.1, 1000 );
-    var canvas = document.getElementById("custom");
+   var material  = new THREE.MeshStandardMaterial()
+   var camera  = new THREE.PerspectiveCamera(45, elmt.clientWidth / window.innerHeight, 0.1, 1000 );
+   var canvas = document.getElementById("custom");
 
-  
-    var renderer  = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
+
+   var renderer  = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
     //renderer.setSize( elmt.clientWidth, window.innerHeight );
 
     //renderer.setSize( canvas.width, canvas.height );
@@ -72,7 +69,7 @@ console.log("remapped scale", remappedScale(3));
 
     var sphere = new THREE.SphereGeometry(0.005, 32, 32);
     var object = new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({
-        color: 0xff0000
+      color: 0xff0000
     }));
 
     //object.name = "sphere";
@@ -83,73 +80,31 @@ console.log("remapped scale", remappedScale(3));
 
     var anim;
 
+    var tooltipEnabledObjects = [];
+
 
 
 
 //-----------update on click-----------
 
- $("#countries-input").on("change", function(e) {
-  //var el = $("#fromto");
-  console.log("countries inout", e);
-
- console.log(document.getElementById('countries-input').value);
-
- var value = document.getElementById('countries-input').value;
+$("#countries-input").on("change", function(e) {
+  var value = document.getElementById('countries-input').value;
   addGlobe(value);
 });
 
-  
+
 
 $(document).ready(function() {
 
-populateDatalist(allDonors);
+  populateDatalist(allDonors);
 
-//var manager = new THREE.LoadingManager({onload:test(), onProgress:test2()});
-//var manager = new THREE.LoadingManager({onProgress:test2(),onload:test()});
-var manager = new THREE.LoadingManager();
-
-
-// manager.onProgress = function (itemsLoaded) { 
-// const loadingScreen = document.getElementById( 'loading-screen' );
-//   //loadingScreen.style.display = 'inline-block';
-//   loadingScreen.classList.add( 'fade-out' );
-//   // config
-
-//   console.log("loading", itemsLoaded);
-
-// }
-
-
-// function test(){
-//   const loadingScreen = document.getElementById( 'loading-screen' );
-//   const main = document.getElementById( 'main' );
-//   //main.style.display = "block";
-//   loadingScreen.style.display = 'none';
-//   loadingScreen.innerHTML = " ";
-//   //material.opacity = 0.2;
-
-//   //loadingScreen.classList.add( 'fade-out' );
-//   console.log("loaded");
-// }
-
-// function test2(itemsLoaded) {
-//    const loadingScreen = document.getElementById( 'loading-screen' );
-//   //loadingScreen.style.display = 'inline-block';
-//   loadingScreen.classList.add( 'fade-out' );
-//   // config
-
-//   console.log("loading", itemsLoaded);
-//   // loadingScreen.innerHTML = " ";
-//   // material.opacity = 0.2;
-
-// }
 
  //console.log("this is a loading manager", manager);
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.autoRotate = false;
-  controls.autoRotateSpeed = -1.0;
-  controls.enablePan = false;
-  controls.enableZoom = false;
+ controls = new THREE.OrbitControls(camera, renderer.domElement);
+ controls.autoRotate = false;
+ controls.autoRotateSpeed = -1.0;
+ controls.enablePan = false;
+ controls.enableZoom = false;
   //makes the sphere rotate "smoother"
   controls.enableDamping = true;
   controls.dampingFactor = 0.15;
@@ -166,8 +121,8 @@ var manager = new THREE.LoadingManager();
   scene.add(ambientLight);
 
   var loaders = {
-    map: new THREE.TextureLoader(manager).load('textures/earth.jpg'),
-    bumpMap: new THREE.TextureLoader(manager).load('textures/earth-bump.jpg')
+    map: new THREE.TextureLoader().load('textures/earth.jpg'),
+    bumpMap: new THREE.TextureLoader().load('textures/earth-bump.jpg')
   }
 
   // material.map   = new THREE.TextureLoader().load('textures/earth.jpg')
@@ -182,7 +137,7 @@ var manager = new THREE.LoadingManager();
   //scene.fog = new THREE.Fog(fogColor, 0.0025, 20);
   material.opacity = 1;
   //lightHolder.quaternion.copy(camera.quaternion);
-  earthMesh.rotation.y = Math.PI;
+  earthMesh.rotation.y = Math.PI*1.5;
   //camera.add(light)
   //camera.add(light1)
 
@@ -194,22 +149,14 @@ var manager = new THREE.LoadingManager();
   //scene.add( axesHelper );
 
   document.addEventListener( 'mousemove', onMouse, false );
+  //console.log("onMouse", onMouse);
   elmt.appendChild( renderer.domElement );
 
-  addGlobe("Switzerland");
+  addGlobe("Germany");
 
-
-
-
-
-  //animate();
-
-    //----this is where the magic happens-------
-  //setInterval(moveObj, 50);});
 })
 
 function addGlobe(country) {
-
 //$("#loading-screen").hide();
 
 
@@ -222,18 +169,18 @@ earthMesh.children = [];
 //var lineGeometry = new THREE.Geometry();
 //var lineMat = new THREE.LineBasicMaterial( { color: '#fcba03', linewidth: 1 } );
 //var mesh, movingGlobe;
-  var mesh, movingGlobe;
-  var lineMat = new THREE.LineBasicMaterial( { color: '#fff', linewidth: 6, scale: 2} );
+var mesh, movingGlobe;
+var lineMat = new THREE.LineBasicMaterial( { color: '#fff', linewidth: 6} );
   //earthMesh.rotation.set(new THREE.Vector3( 0, 0, Math.PI / 2));
 
-   d3.json("data/aiddata.json", function(error, data) {
+  d3.json("data/aiddata.json", function(error, data) {
 
-    earthMesh.rotation.y = Math.random();
+    //earthMesh.rotation.y = Math.random();
     // var mesh, movingGlobe;
     // var lineGeometry = new THREE.Geometry();
     // var lineMat = new THREE.LineBasicMaterial( { color: '#fcba03', linewidth: 1 } );
 
-     console.log("reload data", country);
+    console.log("reload data for", country);
     if (error) throw error;
     for (let i=0;i<data.length;i++){
       if (data[i].Donor == country){
@@ -241,8 +188,8 @@ earthMesh.children = [];
         //console.log("data was loaded and contains", country);
         //lat,long donor / lat,long recip
         var array = [[data[i].lat_donor, data[i].lon_donor],[data[i].lat_recip, data[i].lon_recip]];
-        console.log("recip", data[i].lat_recip, data[i].lon_recip,"donor", data[i].lat_donor, data[i].lon_donor);
-        console.log("convert to cart", converttoCartesian2(radius1, array[0]).y,Math.PI / 2);
+        //console.log("recip", data[i].lat_recip, data[i].lon_recip,"donor", data[i].lat_donor, data[i].lon_donor);
+        //console.log("convert to cart", converttoCartesian2(radius1, array[0]).y,Math.PI / 2);
         
         //rotate earth to correct position:
         //https://stackoverflow.com/questions/34267181/rotating-a-sphere-so-that-clicked-point-is-vector-toward-the-camera?noredirect=1&lq=1
@@ -251,13 +198,17 @@ earthMesh.children = [];
         var tube = new THREE.TubeGeometry(createTube(array), tubeSegments, remappedScale(data[i].Amount), 50, false);
         //console.log(data[i]);
       //var tube = new THREE.TubeGeometry(createTube(coords[i]), 30, (coords[i][3])*0.0001, 50, false);
-        mesh = new THREE.Mesh(tube, new THREE.MeshBasicMaterial({
-              color: 0xff0000,
-              blending: THREE.AdditiveBlending,
-              opacity: 0.9,
-              transparent: true,
-        }));
-      //console.log(data[i].Donor,data[i].Amount);
+      mesh = new THREE.Mesh(tube, new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        blending: THREE.AdditiveBlending,
+        opacity: 0.9,
+        transparent: true,
+      }));
+
+      controls.update();
+
+
+
 
       mesh.name = "Aid money from " + data[i].Donor + " to " + data[i].Recipient;;
       mesh.userData.amount = data[i].Amount;
@@ -266,44 +217,37 @@ earthMesh.children = [];
       movingGlobe = object.clone();
       movingGlobe.name = "Aid money from " + data[i].Donor + " to " + data[i].Recipient;
       movingGlobe.userData = array;
+      movingGlobe.userData.amount = data[i].Amount;
       objectArray.push(movingGlobe);
       //console.log("mesh", mesh, "mesh i", meshArray[i], "full array", meshArray)
         //console.log(e);
-      earthMesh.add(mesh);
+        earthMesh.add(mesh);
       //earthMesh.add(line);
       earthMesh.add(movingGlobe);
       //console.log("mesh", mesh);
-      }
+    }
 
 
 //------------------add lines & labels---------------------------
-//labels: https://codepen.io/dxinteractive/pen/reNpOR
-      var elem = document.createElement('div');
-      elem.style.top = '600px';
-      elem.style.position = 'absolute';
-      elem.style.color = 'white';
-      elem.innerHTML = 'test';
 
-      var lineGeometry = new THREE.Geometry();
-      lineGeometry.vertices.push(converttoCartesian2(radius1, [data[i].lat_donor, data[i].lon_donor]));
-      lineGeometry.vertices.push(converttoCartesian2(radius1 + 0.06, [data[i].lat_donor, data[i].lon_donor]));
-      var line = new THREE.Line( lineGeometry, lineMat );
-      //line.add(elem);
-      //console.log("scene", scene.userData.element);
-      scene.userData.element = elem;
+var lineGeometry = new THREE.Geometry();
+lineGeometry.vertices.push(converttoCartesian2(radius1, [data[i].lat_donor, data[i].lon_donor]));
+lineGeometry.vertices.push(converttoCartesian2(radius1 + 0.06, [data[i].lat_donor, data[i].lon_donor]));
+var line = new THREE.Line( lineGeometry, lineMat );
+
 
       //scene.add(line); 
 
     }  
 
     //console.log("linegeom", lineGeometry, "line", line);
-  
+
     scene.add(earthMesh);
     
     //this prevents the anim from running over and over again
     if (animIsRunning) {
     animate(); //execute animate only once data is loaded!
-        setTimeout(function() {
+    setTimeout(function() {
       const loadingScreen = document.getElementById( 'loading-screen' );
       //loadingScreen.style.display = 'inline-block';
       //console.log("repeat loader");
@@ -314,33 +258,16 @@ earthMesh.children = [];
       //loadingScreen.style.width = "0";
       //loadingScreen.style.height = "0";
 
-    }, 2000);
+    }, 3500);
 
 
-    }
+  }
 
-    animIsRunning = false;
+  animIsRunning = false;
   
 
-    });
+});
   
-
-    // setTimeout(function() {
-    //   const loadingScreen = document.getElementById( 'loading-screen' );
-    //   //loadingScreen.style.display = 'inline-block';
-    //   console.log("repeat loader");
-    //   //loadingScreen.classList.add( 'fade-out' );
-    //   $( '#loading-screen' ).fadeOut("slow");
-    //   //loadingScreen.style.zIndex = "-1";
-    //   $( '#test0' ).show();
-    //   //loadingScreen.style.width = "0";
-    //   //loadingScreen.style.height = "0";
-
-    // }, 1000);
-  //   for (let i=0;i<data.length;i++){  
-
-  //   
-
 
 }
 
@@ -348,16 +275,16 @@ earthMesh.children = [];
 function createTube(coordPair){
       //console.log("selected cords", coordPair[0][0]);
       //two-element array [longitude, latitude]
-  const interpolate = d3.geoInterpolate([coordPair[0][1],coordPair[0][0]], [coordPair[1][1],coordPair[1][0]]);
-  const midCoord0 = interpolate(0.5);
-  const midCoord1 = interpolate(0.25);
-  const midCoord2 = interpolate(0.75);
+      const interpolate = d3.geoInterpolate([coordPair[0][1],coordPair[0][0]], [coordPair[1][1],coordPair[1][0]]);
+      const midCoord0 = interpolate(0.5);
+      const midCoord1 = interpolate(0.25);
+      const midCoord2 = interpolate(0.75);
 
   //console.log("interpolated coords", midCoord1, midCoord2);
   
   function clamp(num, min, max) {
     //if distance start-end is smaller than min
-  return num <= min ? min : (num >= max ? max : num);
+    return num <= min ? min : (num >= max ? max : num);
   }
 
   
@@ -380,15 +307,17 @@ function createTube(coordPair){
 
     );
 
-  console.log("this is the start of the curve", start);
+  //console.log("this is the start of the curve", start);
   return curve;
 
 }
 
 
 function onMouse(event) {
+  //update position of mouse vector as defined in the beginning
   mouse.x = ( event.clientX / elmt.clientWidth ) * 2 - 1;
   mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
 }
 
 
@@ -401,68 +330,52 @@ function animate(d) {
     
     //console.log("objectarray", objectArray);
 
-     if (counter <= 1) {
+    if (counter <= 1) {
         //console.log("counter", counter);
         for (var i=0; i<objectArray.length; i++){
 
-        objectArray[i].position.copy(createTube(objectArray[i].userData).getPointAt(counter) );
+          objectArray[i].position.copy(createTube(objectArray[i].userData).getPointAt(counter) );
         //objectArray[1].position.copy(createTube(coords[5]).getPointAt(counter) );
         //console.log("position of object should be changed", objectArray[0].position, counter)
-        }
-
-        counter += 0.005
-      } else {
-        counter = 0;
       }
+
+      counter += 0.005
+    } else {
+      counter = 0;
+    }
 
     anim=requestAnimationFrame(animate);
     render();
-}
+  }
 
-function render() {
+  function render() {
     controls.update();
     raycaster.setFromCamera( mouse, camera );
-
-    // calculate objects intersecting the picking ray
-    var intersects = raycaster.intersectObjects( earthMesh.children );
+    //console.log("what is mouse", mouse);
 
     //console.log("intersects");
- $('html,body').css('cursor', 'default');
-    if (intersects.length > 0){
-    if (INTERSECTED != intersects[0] ){
-      //below is true when mouse hover line
-      if (INTERSECTED){
-        INTERSECTED.object.material.color.set(0xff0000);
-        $('html,body').css('cursor', 'pointer');
-        //console.log("what's intersecting", INTERSECTED);
-        document.getElementById("selector").innerHTML = INTERSECTED.object.name;
-        //document.getElementById("amount").innerHTML = INTERSECTED.object.userData.amount;
-      }
-      INTERSECTED = intersects[ 0 ];
-      //make item lightly colored
-      INTERSECTED.object.material.color.set(0xFF9090);
+    $('html,body').css('cursor', 'default');
+    onTubeHover();
 
-    } else {
-      //$('html,body').css('cursor', 'pointer');
-       INTERSECTED.object.material.color.set(0xFF9090);
-    }
-    }
 
     lightHolder.quaternion.copy(camera.quaternion);
     renderer.render(scene, camera);
-}
 
 
-function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+
+  }
+
+
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
         //renderer.setSize( 200, 200 );
         renderer.setSize( elmt.clientWidth, window.innerHeight, false );
         renderer.setViewport( elmt.clientWidth, window.innerHeight );
         //renderer.setSize( window.innerWidth, window.innerHeight );
       }
 
-function resizeCanvasToDisplaySize() {
+      function resizeCanvasToDisplaySize() {
   //console.log("canvas", canvas);
   const width = canvas.clientWidth;
   //const height = canvas.clientHeight;
@@ -501,7 +414,7 @@ function converttoCartesian2(R, coord1){
 function populateDatalist(array) {
   var options = '';
   array.forEach( function (i) {
-    
+
     options += '<option value="'+i+'" />';
 
   })
@@ -510,5 +423,86 @@ function populateDatalist(array) {
   dataList.innerHTML = options;
 }
 
-//console.log("converted2", converttoCartesian2(radius1, coord1));
+
+function showTooltip(a,b) {
+  var divElement = $("#tooltip");
+  console.log("show tt");
+  //console.log("intersects in showtooltip()",a);
+
+  if (divElement) {
+    divElement.css({
+      display: "block",
+      opacity: 1.0
+    });
+
+    var canvasHalfWidth = renderer.domElement.offsetWidth / 2;
+    var canvasHalfHeight = renderer.domElement.offsetHeight / 2;
+
+    var tooltipPosition = a.clone().project(camera);
+    tooltipPosition.x = (tooltipPosition.x * canvasHalfWidth) + canvasHalfWidth + renderer.domElement.offsetLeft;
+    tooltipPosition.y = -(tooltipPosition.y * canvasHalfHeight) + canvasHalfHeight + renderer.domElement.offsetTop;
+
+    var tootipWidth = divElement[0].offsetWidth;
+    var tootipHeight = divElement[0].offsetHeight;
+
+    divElement.css({
+      left: `${tooltipPosition.x - tootipWidth/2}px`,
+      top: `${tooltipPosition.y - tootipHeight - 5}px`
+    });
+
+    console.log("amount", b.userData.amount);
+    divElement.text( "Aid amount: $" + b.userData.amount);
+
+  }
+}
+
+
+function hideTooltip() {
+  var divElement = $("#tooltip");
+  console.log("hide tt");
+  if (divElement) {
+    divElement.css({
+      display: "none"
+    });
+  }
+}
+
+function onTubeHover() {
+
+    // calculate objects intersecting the picking ray
+    var intersects = raycaster.intersectObjects( earthMesh.children );
+    if (intersects.length > 0){
+      if (INTERSECTED != intersects[0] ){
+
+      //below is true when mouse hovers line
+      if (INTERSECTED){
+        INTERSECTED.object.material.color.set(0xff0000);
+        console.log("mouse over line", INTERSECTED, mouse);
+        $('html,body').css('cursor', 'pointer');
+        showTooltip(intersects[0].point,intersects[0].object);
+        //console.log("what's intersecting", INTERSECTED);
+
+        //document.addEventListener( 'mousemove', onMouse, false );
+        document.getElementById("selector").innerHTML = INTERSECTED.object.name;
+        //document.getElementById("amount").innerHTML = INTERSECTED.object.userData.amount;
+      }
+      console.log("after mouse over line?");
+      INTERSECTED = intersects[ 0 ];
+      //make item lightly colored
+       //document.addEventListener( 'mousemove', onMouse);
+       INTERSECTED.object.material.color.set(0xff4c4c);
+      //hideTooltip();
+
+    }} else {
+      //$('html,body').css('cursor', 'pointer');
+      if (INTERSECTED){
+
+        INTERSECTED.object.material.color.set(0xff0000);
+        hideTooltip();
+      }
+    }
+
+
+  }
+
 
